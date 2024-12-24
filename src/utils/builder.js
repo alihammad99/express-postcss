d9fe88930cd13378952411245820afd30146da04
Ali Hammad <aliressan.ar@gmail.com>
2024-12-24 22:57:53 +0300

    Add cssnano for CSS minification in builder.js and update package.json

--- src/utils/builder.js
+++ src/utils/builder.js
@@ -2,6 +2,7 @@ import { promises as fs } from "fs";
 import path from "path";
 import postcss from "postcss";
 import tailwindcss from "tailwindcss";
+import cssnano from "cssnano";
 
 export const buildCSS = async (req, res) => {
   try {
@@ -12,12 +13,13 @@ export const buildCSS = async (req, res) => {
     // Path to the Tailwind configuration file
     const tailwindConfigPath = path.resolve("sample/tailwind.config.js");
 
-    // Process the CSS with PostCSS
+    // Process the CSS with PostCSS and minify it
     const result = await postcss([
       tailwindcss({ config: tailwindConfigPath }), // Specify the Tailwind config
+      cssnano(), // Add cssnano for minification
     ]).process(css, { from: cssFilePath, to: "output.css" });
 
-    // await fs.writeFile("output.css", result.css); // Uncomment to save to disk
+    await fs.writeFile("output.css", result.css); // Uncomment to save to disk
 
     console.log("✨ CSS file built successfully");
     res.set("Content-Type", "text/css"); // Set the content type to CSS
